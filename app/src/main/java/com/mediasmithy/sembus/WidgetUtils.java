15c3d8a90dece7ea90d7959fed467efaa9fb4987
Vasilevich Roman <roman@mediasmithy.com>
2015-11-16 01:02:45 +0300

    Moving the legacy project  there.

---
+++ app/src/main/java/com/mediasmithy/sembus/WidgetUtils.java
@@ -0,0 +1,6 @@
+package com.mediasmithy.sembus;
+
+public class WidgetUtils {
+    final static String WIDGET_UPDATE_ACTION ="com.mediasmithy.intent.action.UPDATE_WIDGET";
+    final static String APP_TAG = "SEMbus";
+}
